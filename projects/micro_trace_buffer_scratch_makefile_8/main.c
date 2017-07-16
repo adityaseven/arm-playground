f69bb6f52a6b981d0cdb230f30714e2cad708fbe
Raghava Aditya Renukunta <adi.raghava3@gmail.com>
2017-07-15 17:37:19 -0700

    micro misc changes

--- projects/micro_trace_buffer_scratch_makefile_8/main.c
+++ projects/micro_trace_buffer_scratch_makefile_8/main.c
@@ -17,66 +17,86 @@
  */
 
 #include <MKL46Z4.h>
-
-#define RED_LED   29U
-#define GREEN_LED 5U
-#define RPORT  PORTE
-#define GPORT  PORTD
-#define RGPIO  GPIOE
-#define GGPIO  GPIOD
-
-#define SW1		3U
-#define SW3		12U
-#define S1PORT  PORTC
-#define S3PORT  PORTC
-#define S1GPIO  GPIOC
-#define S3GPIO  GPIOC
+#include <led.h>
+#include <switch.h>
 
 #define MTB_SIZE 0x1000
 
 #define MTB_ALIGNMENT 64
-unsigned char __attribute__((section (".mtb_buf"))) mtb_buf[MTB_SIZE] __attribute__ ((aligned (MTB_ALIGNMENT)));
+//unsigned char __attribute__((section (".mtb_buf"))) mtb_buf[MTB_SIZE] __attribute__ ((aligned (MTB_ALIGNMENT)));
 
 void setup_mtb()
 {
-	MTB->MASTER = MTB_MASTER_EN(1) | MTB_MASTER_MASK(7);
+	MTB->MASTER = MTB_MASTER_EN(1) | MTB_MASTER_MASK(8);
 }
 
-void init_leds()
+void delay(uint32_t c)
 {
-	SIM->SCGC5 |= (SIM_SCGC5_PORTD_MASK |
-					SIM_SCGC5_PORTE_MASK);
+	volatile  uint32_t i,j;
 
-	GPORT->PCR[GREEN_LED] |= PORT_PCR_MUX(1U);
-	RPORT->PCR[RED_LED]   |= PORT_PCR_MUX(1U);
-
-	GGPIO->PDDR |= (1U << GREEN_LED);
-	RGPIO->PDDR |= (1U << RED_LED);
-
-	GGPIO->PSOR |= (1U << GREEN_LED);
-	RGPIO->PSOR |= (1U << RED_LED);
+	for (i = 0U; i < 1000U; i++) {
+		for (j = 0U; j < c; j++) {
+			__asm__("nop");
+		}
+	}
 }
 
-void init_switches()
+void sequence0()
 {
-	SIM->SCGC5 |= (SIM_SCGC5_PORTC_MASK);
+	RON;
+	GON;
+	delay(4000);
+	ROFF;
+	GOFF;
+	delay(4000);
+}
 
-	S1PORT->PCR[SW1] |= PORT_PCR_MUX(1U);
-	S3PORT->PCR[SW3] |= PORT_PCR_MUX(1U);
+void sequence1()
+{
+	RON;
+	GON;
+	delay(2000);
+	ROFF;
+	GOFF;
+	delay(2000);
+}
 
-	S1PORT->PCR[SW1] |= (PORT_PCR_PE(1U) | PORT_PCR_PS(1U));
-	S3PORT->PCR[SW3] |= (PORT_PCR_PE(1U) | PORT_PCR_PS(1U));
-
-	S1GPIO->PDDR &= ~(1U << SW1);
-	S3GPIO->PDDR &= ~(1U << SW3);
+void sequence2()
+{
+	RON;
+	delay(200);
+	GON;
+	delay(200);
+	ROFF;
+	delay(200);
+	GOFF;
+	delay(200);
 }
 
 int main()
 {
-
+	uint8_t sw_count = 0;
 	setup_mtb();
 	init_leds();
 	init_switches();
+
+	while (1) {
+		sw_count++;
+
+		switch(sw_count) {
+		case 1:
+			sequence0();
+			break;
+		case 2:
+			sequence1();
+			break;
+		case 3:
+			sequence2();
+			break;
+		default:
+			sw_count = 0;
+		}
+	}
 
 
 	return 0;
